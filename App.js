62bd458d0d45bb7ffbb33bd7b9cabb06c5009bf9
OzanDuru <112401899+OzanDuru@users.noreply.github.com>
2025-12-05 20:37:26 +0300

    feat: add icons and custom styling to navigation (Part 5)

--- App.js
+++ App.js
@@ -5,24 +5,47 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Settings } from './screens/Settings';
 import Home from './screens/Home';
 import Details from './screens/Details';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function HomeStack() {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Details" component={DetailsScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: 'tomato' },
+        headerTintColor: '#fff'
+      }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Details" component={Details} />
     </Stack.Navigator>
   );
 }
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Explore') {
+              iconName = focused ? 'map' : 'map-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+            // Return the appropriate <Ionicons> icon
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarActiveTintColor="tomato"
+        tabBarInactiveTintColor="gray"
+      >
         <Tab.Screen name="Explore" component={HomeStack} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
   );
